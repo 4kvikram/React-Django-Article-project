6b5fdb81f1f87aafbb0ade93712969920ab0281f
vikram <4kvikram@gmail.com>
2020-08-18 16:55:19 +0530

    first commit

---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,43 @@
+from rest_framework import viewsets
+from articles.models import Article
+from .serializers import ArticleSerializers
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializers
+    queryset = Article.objects.all()
+
+
+
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     DestroyAPIView,
+#     UpdateAPIView
+# )
+#
+#
+#
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+#
+#
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+#
+#
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+#
+#
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+#
+#
+# class ArticleDestroyView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
